bc92f6a0078d0b54bbb74f081f51854a9c71d389
ALewis91 <48857379+ALewis91@users.noreply.github.com>
2019-09-06 18:45:47 -0700

    Create Entry.h

---
+++ ListPQ/Entry.h
@@ -0,0 +1,19 @@
+pragma once
+#include <string>
+class Entry 
+{				
+private:						// private data
+	int _key;						// key
+	std::string _value;					// value
+public:		
+	Entry(const int& k, const std::string &v)	// constructor
+	{
+		_key = k;
+		_value = v;
+	}
+	const int key() const { return _key; }		// get key
+	const std::string value() const { return _value; }	// get value
+	void setKey(const int& k) { _key = k; }		// set key
+	void setValue(const std::string& v) { _value = v; }	// set value
+
+};
